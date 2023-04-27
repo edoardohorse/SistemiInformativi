dfd82c1633d8bc0e1f217d7e137e0d697d6c3a0c
Edoardo Cavallo <iweek16@gmail.com>
2023-04-27 13:19:03 +0200

    Aggiunto la connect ad altervista

--- php/connect.php
+++ php/connect.php
@@ -5,12 +5,22 @@ $username = "root";
 $password = "";
 $db = "giardinaggio";
 
+$servername_altervista = "localhost";
+$username_altervista = "civicsens";
+$password_altervista = "giardinaggio30L";
+$db_altervista = "my_civicsens";
+
 // Create connection
 
 
 global $conn;
 
-$conn = new mysqli($servername, $username, $password, $db);
+if(str_contains($_SERVER['SERVER_NAME'], 'altervista')){
+    $conn = new mysqli($servername_altervista, $username_altervista, $password_altervista, $db_altervista);
+}
+else{
+    $conn = new mysqli($servername, $username, $password, $db);
+}
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
